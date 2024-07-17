d7f2bbd7cef1a1d343d6f5e278c203c78c2b795b
Wagner Ferreira Freitas <wagnerffreitas1973@gmail.com>
2024-07-16 21:43:23 -0300

    Inclusão de lugares biblicos no jogo

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         button.classList.toggle('clicked')
-        window.open('Alimentos/forca.html', '_self');
+        window.open('Alimentos/alimentos.html', '_self');
+        window.open('Lugaresbiblicos/lugaresbiblicos.html', '_self');
     });
 });
