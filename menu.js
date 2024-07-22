5b6b9d8b9baf90e33fa4d670eaf09c4df52eb4ea
Wagner Ferreira Freitas <wagnerffreitas1973@gmail.com>
2024-07-22 13:44:21 -0300

    Correção do menu

--- menu.js
+++ menu.js
@@ -4,21 +4,21 @@ cards.forEach(card => {
     card.addEventListener('click', () => {
         let url;
         if (card.classList.contains('alimentos')) {
-            url = 'alimentos/alimentos.html';
+            url = '/alimentos/alimentos.html';
         } else if (card.classList.contains('lugaresbiblicos')) {
-            url = 'lugaresbiblicos/lugaresbiblicos.html';
+            url = '/lugaresbiblicos/lugaresbiblicos.html';
         } else if (card.classList.contains('nomebiblicos')) {
-            url = 'nomebiblicos/nomebiblicos.html';
+            url = '/nomebiblicos/nomebiblicos.html';
         } else if (card.classList.contains('nomejogos')) {
-            url = 'nomejogos/nomejogos.html';
+            url = '/nomejogos/nomejogos.html';
         } else if (card.classList.contains('nomecarros')) {
-            url = 'nomecarros/nomecarros.html';
+            url = '/nomecarros/nomecarros.html';
         } else if (card.classList.contains('timefutebol')) {
-            url = 'timefutebol/timefutebol.html';
+            url = '/timefutebol/timefutebol.html';
         } else if (card.classList.contains('dicionario')) {
-            url = 'dicionario/dicionario.html';
+            url = '/dicionario/dicionario.html';
         } else if (card.classList.contains('nomepessoas')) {
-            url = 'nomepessoas/nomepessoas.html';
+            url = '/nomepessoas/nomepessoas.html';
         }
 
         if (url) {
